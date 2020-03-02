5f85a1096459271c9201e8d79a321109d5731245
ramhemanth3 <ram.hemanth3@gmail.com>
2020-03-02 13:32:19 +0530

    Update htext4.py

--- htext4.py
+++ htext4.py
@@ -4,8 +4,8 @@ import urllib.request
 from urllib.request import urlopen, Request
 user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
 headers={'User-Agent':user_agent,}
-#url1 = str(sys.argv[1]) 
-url1 = 'http://www.english-for-students.com/banished-son-saves-his-brothers.html'
+url1 = str(sys.argv[1]) 
+#url1 = 'http://www.english-for-students.com/banished-son-saves-his-brothers.html'
 request=urllib.request.Request(url1,None,headers) #The assembled request
 response = urllib.request.urlopen(request)
 text = response.read()
@@ -15,6 +15,7 @@ for script in soup(["script", "style"]): #You need to extract this <script> and 
 text = soup.getText() #this is the method that I had like 40 min problems
 text = text.encode('utf-8') #make sure to encode your text to be compatible
 print(text)
+
 
 '''
 html = urllib.request.urlopen(url).read() #make the request to the url
